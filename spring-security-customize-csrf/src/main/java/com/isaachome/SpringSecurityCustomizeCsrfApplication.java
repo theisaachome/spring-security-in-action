515af8671eb829502819ac6a75cc98130fb59911
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-07-21 23:12:11 +0630

    left behind

---
+++ spring-security-customize-csrf/src/main/java/com/isaachome/SpringSecurityCustomizeCsrfApplication.java
@@ -0,0 +1,13 @@
+package com.isaachome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCustomizeCsrfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCustomizeCsrfApplication.class, args);
+	}
+
+}
